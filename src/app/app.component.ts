26f2bfed3ec14dc0e8ac655f0d539044c376db46
kiran <kiranreddybudati503@gmail.com>
2019-02-18 16:32:53 +0530

    make it better

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+const template = require('./app.component.html');
+
+@Component({
+    selector: 'app-home',
+    template
+})
+export default class AppComponent {
+    welcomeMessage = 'Welcome';
+
+    async run() {
+        /**
+         * Insert your Outlook code here
+         */
+    }
+}
